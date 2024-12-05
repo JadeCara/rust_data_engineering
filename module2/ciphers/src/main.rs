752b19da820cab22b0ff5e08d70eeb81328c7529
Jade Wibbels <jade@Jades-MBP.local>
2024-12-05 12:39:27 -0700

    added decoder ring args and call

--- module2/ciphers/src/main.rs
+++ module2/ciphers/src/main.rs
@@ -12,10 +12,16 @@ To decrypt:
 
 cargo run --  --message "Ypp dy dro lexuob. Ofobi zobcyx pyb drowcovfoc" --decrypt --shift 10
 
+To Guess: 
+
+cargo run -- --message "Ypp dy dro lexuob. Ofobi zobcyx pyb drowcovfoc" --guess 
+
 */
 
 use cipher_makers::{decrypt, encrypt, homophonic_cipher};
 use clap::Parser;
+
+mod decoder_ring;
 
 /// CLI tool to encrypt and decrypt messages using the caeser cipher
 #[derive(Parser, Debug)]
@@ -34,9 +40,17 @@ struct Args {
     #[arg(short, long)]
     decrypt: bool,
 
+    /// Guess the shift for the message
+    #[arg(short, long)]
+    guess: bool,
+
     /// The message to encrypt or decrypt
     #[arg(short, long)]
     message: String,
+
+    //statistical information about the message
+    #[arg(short, long)]
+    printstats: bool,
 
     /// The shift to use for the cipher
     /// Must be between 1 and 25, the default is 3
@@ -54,7 +68,17 @@ struct Args {
         println!("{}", encrypt(&args.message, args.shift));
     } else if args.decrypt {
         println!("{}", decrypt(&args.message, args.shift));
+    } else if args.guess {
+        if args.printstats {
+            decoder_ring::print_stats_analysis(&args.message);
+        }
+        let (depth, best_shift, decrypted, max_score) = decoder_ring::guess_shift(&args.message, 26);
+        println!(
+            "Best shift: {} (out of {}), score: {}",
+            best_shift, depth, max_score
+        );
+        println!("Decrypted message: {}", decrypted); 
     } else {
-        println!("Please specify either --encrypt or --decrypt");
+        println!("Please specify mode: --ahomophone, --encrypt, --decrypt, or --guess");
     }
  }
